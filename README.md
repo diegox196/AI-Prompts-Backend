d8671c6ea8f36ea2e4e63f6f1144b2031b5e486b
diego <despedessol@gmail.com>
2023-07-03 16:49:31 -0600

    Initial structure

---
+++ README.md
@@ -0,0 +1,17 @@
+# AI Prompts
+
+Allows you to create queries using the [OpenAI API](https://platform.openai.com/docs/api-reference/introduction).
+
+## Building and Running
+
+
+## Resources
+**/users**
+**/session**
+
+
+## How to
+
+you can fetch data with any kind of methods you know(fetch API, Axios, jquery ajax,...)
+
+### Example
---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const PORT = 3201;
+
+// database connection
+const mongoose = require("mongoose");
+const db = mongoose.connect("mongodb://127.0.0.1:27017/aipromts", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
+
+// parser for the request body (required for the POST and PUT methods)
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+// check for cors
+const cors = require("cors");
+app.use(cors({
+  domains: '*',
+  methods: "*"
+}));
+
+app.listen(PORT, () => {
+  console.log(`The server is listening on the PORT ${PORT} (URL http://localhost:${PORT} )`);
+})
