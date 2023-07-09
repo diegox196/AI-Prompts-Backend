e5d6a4fb5df1d1fc1f9366a0ac5bb7c16ffc7d01
diego <despedessol@gmail.com>
2023-07-08 20:41:29 -0600

    Add username attribute

--- models/userModel.js
+++ models/userModel.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   email: {type: String},
+  username: {type: String},
   first_name: {type: String},
   last_name: {type: String},
   password: {type: String},
